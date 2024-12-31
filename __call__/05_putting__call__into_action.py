2d9e9710790e6bb6523400aba5727d0ccb8491f5
sindresb1 <sindre.torgals@samspar.no>
2024-12-31 09:28:16 +0000

    la til nytt eksempel

--- __call__/05_putting__call__into_action.py
+++ __call__/05_putting__call__into_action.py
@@ -4,3 +4,32 @@
 # Retain state between calls
 # Cache values that result from previous computations
 # Implement straightforward and convenient APIs
+
+# Writing Stateful Callables
+def cumulative_average():
+    data = []
+    def average(new_value):
+        data.append(new_value)
+        return sum(data) / len(data)
+    return average
+
+stream_average = cumulative_average()
+
+print(stream_average(12))
+print(stream_average(13))
+print(stream_average(11))
+print(stream_average(10))
+
+class CumulativeAverager:
+    def __init__(self):
+        self.data = []
+    def __call__(self, new_value):
+        self.data.append(new_value)
+        return sum(self.data) / len(self.data)
+
+stream_average = CumulativeAverager()
+print(stream_average(12))
+print(stream_average(13))
+print(stream_average(11))
+print(stream_average(10))
+print(stream_average.data)
